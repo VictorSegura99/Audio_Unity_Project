b24278e6698a784aa77de1fea9d8843fe9e79683
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2020-05-12 17:19:06 +0200

    Updated mixer

--- WAG_No_Sound/Assets/Scripts/Audio/AudioAmbient.cs
+++ WAG_No_Sound/Assets/Scripts/Audio/AudioAmbient.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class AudioAmbient : MonoBehaviour
 {
-    public AudioClip audioClip;
-    public float minDistance = 0.8f;
-    public float maxDistance = 2.9f;
-
+    public bool isSpatial = true;
+    public bool randomDelay = true;
     private AudioSource audioSource;
     private AudioManager audioManager;
     void Start()
     {
         audioManager = FindObjectOfType<AudioManager>();
-        audioSource = gameObject.AddComponent<AudioSource>();
-        audioSource.outputAudioMixerGroup = audioManager.sfxGroup;
-        audioSource.clip = audioClip;
-        audioSource.loop = true;
-        audioSource.minDistance = minDistance;
-        audioSource.maxDistance = maxDistance;
-        audioSource.spatialBlend = 1.0f;
-        audioSource.volume = 0.8f;
-        audioSource.PlayDelayed(Random.Range(0.0f, 4.0f));
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.outputAudioMixerGroup = audioManager.worldGroup;
+
+        if (isSpatial)
+        {
+            audioSource.spatialBlend = 1.0f;
+            audioSource.rolloffMode = AudioRolloffMode.Linear;
+        }
+        if (randomDelay)
+        {
+            audioSource.PlayDelayed(Random.Range(0.0f, 4.0f));
+        }
+        else
+        {
+            audioSource.Play();
+        }
     }
 }
--- WAG_No_Sound/Assets/Scripts/AudioManager.cs
+++ WAG_No_Sound/Assets/Scripts/AudioManager.cs
@@ -7,17 +7,24 @@ public class AudioManager : MonoBehaviour
 {
     public AudioMixer audioMixer;
     [HideInInspector]public AudioMixerGroup masterGroup;
-    [HideInInspector]public AudioMixerGroup sfxGroup;
     [HideInInspector]public AudioMixerGroup musicGroup;
+    [HideInInspector] public AudioMixerGroup sfxGroup;
+    [HideInInspector]public AudioMixerGroup uiGroup;
+    [HideInInspector]public AudioMixerGroup worldGroup;
     [HideInInspector]public AudioMixerSnapshot indoorSnapshot;
     [HideInInspector]public AudioMixerSnapshot outdoorSnapshot;
 
     void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+        masterGroup = audioMixer.FindMatchingGroups("Master")[0];
+        musicGroup = audioMixer.FindMatchingGroups("Music")[0];
         sfxGroup = audioMixer.FindMatchingGroups("Sfx")[0];
+        worldGroup = audioMixer.FindMatchingGroups("World")[0];
+        uiGroup = audioMixer.FindMatchingGroups("Ui")[0];
         indoorSnapshot = audioMixer.FindSnapshot("Indoor");
         outdoorSnapshot = audioMixer.FindSnapshot("Outdoor");
+        
     }
     public void PlayerIndoor() // When player enter ReverbZone
     {
