161afa33e1d92cf8da997641fd4fe0f297986c88
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2020-05-12 14:02:00 +0200

    Added Player audio zone detector
    f

---
+++ WAG_No_Sound/Assets/Game Assets/Player/Scripts/PlayerReverbDetector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerReverbDetector : MonoBehaviour
+{
+    private AudioManager audioManager;
+    private bool isInDoor = false;
+    private bool detectedZone = false;
+    private void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+
+    private void Update()
+    {
+        if (!detectedZone && isInDoor)
+        {
+            audioManager.PlayerOutdoor();
+            isInDoor = false;
+        }
+            
+        if (detectedZone && !isInDoor)
+        {
+            isInDoor = true;
+            audioManager.PlayerIndoor();
+        }
+        
+        detectedZone = false;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag.Equals("ReverbZone"))
+            detectedZone = true;
+    }
+
+}
