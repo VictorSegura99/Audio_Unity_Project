4f8894fe5613eba3510d3c3b1480c79ef1784c11
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-05-12 10:10:18 +0200

    MainMenu music sounding kinda lmao

---
+++ WAG_No_Sound/Assets/Scripts/MainMenuMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuMusic : MonoBehaviour
+{
+    AudioSource ac;
+    AudioSource pre_intro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ac = GetComponent<AudioSource>();
+        pre_intro = gameObject.transform.GetChild(0).GetComponent<AudioSource>();
+        pre_intro.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pre_intro.isPlaying && !ac.isPlaying) 
+        {
+            ac.Play();
+        }
+    }
+}
