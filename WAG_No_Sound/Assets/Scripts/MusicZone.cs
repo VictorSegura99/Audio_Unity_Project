a2375f16befa53ae70b2bf4bfde528f6c01a3bcc
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-05-12 18:47:38 +0200

    Music Zones work
    Night/Day different musics still missing

---
+++ WAG_No_Sound/Assets/Scripts/MusicZone.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+
+public class MusicZone : MonoBehaviour
+{
+    MusicManager music_manager;
+
+    public MusicManager.Music_Zone zone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        music_manager = transform.parent.GetComponent<MusicManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            music_manager.actual_zone = zone;
+        }
+    }
+}
